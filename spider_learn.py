e6eccb4d697ca770375bbb2b9a5ad3ff0ce4a694
yc <yucanagy@gmail.com>
2018-05-17 16:05:32 +0800

    set 0

--- spider_learn.py
+++ spider_learn.py
@@ -1,7 +1,7 @@
 import numpy
 import cv2
 
-img = cv2.imread('images/5.jpg',1)
+img = cv2.imread('images/5.jpg', 0)
 cv2.namedWindow('window_name',cv2.WINDOW_AUTOSIZE)
 cv2.imshow('window_name',img)
 cv2.waitKey(0)
